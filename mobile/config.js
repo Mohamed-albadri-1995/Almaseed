de28bd1b5e0e096eaaef25b27765a81f724eaf2c
Mohamed-albadri-1995 <MOHAMED.A.ALBADRI@GMAIL.COM>
2026-09-09 19:32:42 +0300

    Point mobile web screens to Almaseed domain

--- mobile/config.js
+++ mobile/config.js
@@ -1,6 +1,6 @@
 // Base URL of the الطريقة السمّانية backend. Change this one line if the site
 // moves to a different domain.
-export const API_BASE = 'https://almaseeed.com';
+export const API_BASE = 'https://almaseed.com';
 
 // Web pages opened inside the app for the admin/contributor options.
 export const ADMIN_URL = `${API_BASE}/admin`;
@@ -10,4 +10,4 @@ export const CONTRIBUTOR_URL = `${API_BASE}/account`;
 // obvious on the device which build is actually installed (shown on the
 // account screen). If this number does not change after an update, the new
 // APK did not install.
-export const BUILD = 21;
+export const BUILD = 22;
